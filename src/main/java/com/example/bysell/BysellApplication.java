9d92577afd97813a7ef9ba79adfc54eb87a1ceaf
Khanafin Timur <mindiisky@yandex.ru>
2024-03-27 19:31:36 +0300

    les1

---
+++ src/main/java/com/example/bysell/BysellApplication.java
@@ -0,0 +1,13 @@
+package com.example.bysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class BysellApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BysellApplication.class, args);
+	}
+
+}
